b2ae7cdab212c79fd6f66a84aae6f9554bc36c38
Alice Manguinho <118902832+AliceManguinho@users.noreply.github.com>
2022-12-06 14:33:56 -0300

    ajuda
    mexi

---
+++ src/ajuda.svelte
@@ -0,0 +1,10 @@
+<h1>AJUDA</h1>
+
+<h3>Como jogar?</h3>
+<ul>
+    <li>Ir para frente</li>
+    <li>Ir para trás</li>
+    <li>Ir para a direita</li>
+    <li>Ir para a esquerda</li>
+</ul>
+<img src="https://toppng.com/uploads/preview/up-down-left-right-arrow-11550125595yj9avx09ib.png" alt=''>
