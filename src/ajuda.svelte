74e7676344b176edbd71c28645ef504a2c745244
Alice Manguinho <118902832+AliceManguinho@users.noreply.github.com>
2022-12-06 16:14:01 -0300

    Add files via upload
    desgraça

--- src/ajuda.svelte
+++ src/ajuda.svelte
@@ -1,10 +1,30 @@
+<script>
+    import { bubble } from "svelte/internal";
+
+</script>
 <h1>AJUDA</h1>
 
-<h3>Como jogar?</h3>
+<h2>Como jogar?</h2>
 <ul>
     <li>Ir para frente</li>
     <li>Ir para trás</li>
     <li>Ir para a direita</li>
     <li>Ir para a esquerda</li>
 </ul>
-<img src="https://toppng.com/uploads/preview/up-down-left-right-arrow-11550125595yj9avx09ib.png" alt=''>
+
+
+<ul><button>^</button></ul>
+<ul><button>-</button> <button>></button></ul>
+<ul><button>v</button></ul>
+
+<h3>Olá, querido humano.</h3>
+<p>Se você chegou aqui, temo que já esteja em um território perigoso.</p>
+<p>Humanos não deveriam arriscar tanto suas vidas, será que sua curiosidade vale tanto assim?</p>
+<p>Deseja um conselho?</p>
+<p>Não seja guiado por suas emoções,</p>
+<p>o tempo não é seu amigo.</p>
+<p>Não olhe para trás ou tente voltar.</p>
+<p>Fique atento e ouça bem o que lhe rodeia.</p>
+
+<h4>Sua curiosidade pode ser sua maior perdição, 
+    preste atenção nas entrelinhas e proteja sua retaguarda.</h4>
