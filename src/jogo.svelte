0f11f08915d46643cb21b9b8e1e423a4ede1881f
Guilherme Valença <56165820+Guilhermevalenca@users.noreply.github.com>
2022-12-06 16:13:32 -0300

    atualização
    New logica add no game

--- src/jogo.svelte
+++ src/jogo.svelte
@@ -13,4 +13,11 @@
     }function movimentacao(jogador){
         //função chamada sempre quando o usuario fizer um movimento
     }
+    function posicaoinicial(jogador){
+        for(let i in mapa[0]){
+            if(mapa[0][i] == 0){
+                //posição inicial do jogador
+            }
+        }
+    }
 </script>
