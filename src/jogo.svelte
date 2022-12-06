171c1e623e8b169f4166976f9450cee832850010
Guilherme Valença <56165820+Guilhermevalenca@users.noreply.github.com>
2022-12-06 14:26:00 -0300

    teste
    padrao do jogo

---
+++ src/jogo.svelte
@@ -0,0 +1,16 @@
+<script>
+    let mapa = [
+        [1,0,1],
+        [0,0,1],
+        [0,1,1]
+    ];
+    function posicao(jogador){
+        if(mapa[i][j] == 0){
+            //devera andar
+        }else{
+            //nao podera passar
+        }
+    }function movimentacao(jogador){
+        //função chamada sempre quando o usuario fizer um movimento
+    }
+</script>
