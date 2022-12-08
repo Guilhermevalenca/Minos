25a5862b4f225b989d3749ae6b79594d15f5f4d0
Guilhermevalenca <guilherm3vrp@gmail.com>
2022-12-08 20:10:32 -0300

    mapa do jogo

---
+++ logica-do-jogo.js
@@ -0,0 +1,21 @@
+let mapa = [
+    [1,1,0,1,1,1],
+    [2,3,5,0,1,1],
+    [2,1,3,0,1,1],
+    [1,1,0,0,0,1],
+    [2,1,0,1,0,1],
+    [1,0,0,1,0,0],
+    [6,0,1,2,3,1]
+];
+//movimentação
+if(mapa[i][j] == 4){
+    //jogador venceu    
+}
+else if(mapa[i][j] == 0){
+    //posição do jogador passar a ser mapa[i][j]
+}else{
+    //nao pode passar
+}
+//personagem na posição tal ele vai poder andar ou nao
+
+{mapa[0][0] != 0} //paredes ou objetos
