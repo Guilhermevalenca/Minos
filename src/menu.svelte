3b3d94160bf74ee520f7a3af9f416ef837a430df
Guilherme Valença <56165820+Guilhermevalenca@users.noreply.github.com>
2023-01-11 14:00:00 -0300

    Leves alterações
    Tentativa falha de atualização e correção de trechos irrelevantes

--- src/menu.svelte
+++ src/menu.svelte
@@ -1,10 +1,8 @@
 <script>
 	import { trocarEstadoDoJogo } from './Estado.js'
-    import { contador, resertar } from './logica-jogo.js'
-   
     let estadoAtual = 0;
 </script>
-<head>
+<head> 
     <link rel="stylesheet" href="/css/menu.css">
 </head>
 
@@ -14,7 +12,7 @@
 {#if (estadoAtual == 0)}
 
 <div class="divapp">
-    <ul class="ulapp"><button  class="buttonapp" on:click={() => {estadoAtual = 1;trocarEstadoDoJogo('jogar')}}><p class="pmenu">Jogar</p></button></ul>
+    <ul class="ulapp"><button  class="buttonapp" on:click={() => {trocarEstadoDoJogo('jogar')}}><p class="pmenu">Novo jogo</p></button></ul>
     <ul class="ulapp"><button  class="buttonapp" on:click={() => trocarEstadoDoJogo('sobre')}><p class="pmenu">Sobre</p></button></ul>
     <ul class="ulapp"><button  class="buttonapp" on:click={() => trocarEstadoDoJogo('ajuda')}><p class="pmenu">Ajuda</p></button></ul>
 </div>
@@ -22,7 +20,7 @@
 {:else if (estadoAtual == 1)}
 <div class="divapp">
     <ul class="ulapp"><button  class="buttonapp" on:click={() => trocarEstadoDoJogo('jogar')}><p class="pmenu">Continuar</p></button></ul>
-    <ul class="ulapp"><button  class="buttonapp" on:click={() => {resertar();trocarEstadoDoJogo('jogar')}}><p class="pmenu">Jogar</p></button></ul>
+    <ul class="ulapp"><button  class="buttonapp" on:click={() => {trocarEstadoDoJogo('jogar')}}><p class="pmenu">Novo jogo</p></button></ul>
     <ul class="ulapp"><button  class="buttonapp" on:click={() => trocarEstadoDoJogo('sobre')}><p class="pmenu">Sobre</p></button></ul>
     <ul class="ulapp"><button  class="buttonapp" on:click={() => trocarEstadoDoJogo('ajuda')}><p class="pmenu">Ajuda</p></button></ul>
 </div>
