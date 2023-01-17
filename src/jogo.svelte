08715b03365bced98d8208f06c202f23354fc540
Emmily kathylen <emmilysouzakathylen@gmail.com>
2023-01-17 11:50:00 -0300

    texto

--- src/jogo.svelte
+++ src/jogo.svelte
@@ -400,9 +400,9 @@
 {#if !enigma}
 
 
-    <p class='Enigma'>Sempre que vc conseguir passar de uma fase, havera um enigma para ser solucionado, lembre-se: a um limite de tempo, tanto para resolver os enigmas como para sair de cada labirinto, toda vez que vc perder, voltará para o primeiro nivel.</p>
-    <p class="Enigma">OBS: lembre-se tambem que so serão aceitas letras maiusculas nas respostas de todo e qualquer enigma. Nenhuma das palavras chave contera qualquer acento.</p>
-    <p class="Enigma">Apos compreender o funcionamento do Minos Labyrinth, digite: "OK" e poderá prosseguir para a primeira fase.</p>
+    <p class='Enigma'>Sempre que você conseguir passar de uma fase, haverá um enigma para ser solucionado, lembre-se: a um limite de tempo, tanto para resolver os enigmas como para sair de cada labirinto, toda vez que vocêc perder, voltará para o primeiro nivel.</p>
+    <p class="Enigma">OBS: lembre-se tambem que so serão aceitas letras maiusculas nas respostas de todo e qualquer enigma. Nenhuma das palavras chave conterá qualquer acento.</p>
+    <p class="Enigma">Após compreender o funcionamento do Minos Labyrinth, digite: "OK" e poderá prosseguir para a primeira fase.</p>
 <input bind:value={PalavraChave} on:keydown={Alterando(PalavraChave == "OK")} placeholder="APENAS LETRAS MAIUSCULAS" class='RespostaEnigma'>
     
 
