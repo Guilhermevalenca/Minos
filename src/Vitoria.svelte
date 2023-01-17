e7db0491d186f1e3b560b964f42e319df6297fbc
Emmily kathylen <emmilysouzakathylen@gmail.com>
2023-01-17 15:42:02 -0300

    vitoria

--- src/Vitoria.svelte
+++ src/Vitoria.svelte
@@ -18,15 +18,16 @@
         text-align: center;
         font-family: 'Felix Titling';
         font-size: 400%;
-         background: linear-gradient(to bottom, #996600 0%, #ffff00 80%);
+        background: linear-gradient(to bottom, #996600 0%, #ffff00 80%);
         background-clip: text;
         -webkit-background-clip: text;
         color: transparent;
     }
 </style>
 
-<h1>Parabéns, você venceu!!!</h1>
+<h1>Parabéns, você conseguiu sair do labirinto!!!</h1>
 
-<p>Obrigado por jogar nosso jogo, espero que tenha gostado</p>
+<p>Obrigado por jogar nosso jogo, espero que tenha gostado dessa experiência</p>
+<p>Nos veremos em breve, certamente...</p>
 
 
